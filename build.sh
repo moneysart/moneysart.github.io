533f6ab9892348d68cee6b3258c1df476c1132b7
clumdee <chatdanai.l@gmail.com>
2024-04-13 23:56:34 +0700

    run on cloudflare-pages

---
+++ build.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# Set default locale for the environment
+export LC_ALL="C.UTF-8"
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
+
+# build jekyll artifacts
+bundle exec jekyll build
